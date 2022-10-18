5226116202aa5fc15ba38d4349030e2f27e0e6c3
yohannes11 <johannes.neg@gmail.com>
2022-10-18 09:29:12 +0300

    Time: 34 ms (23.99%), Space: 13.5 MB (34.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,5 @@
 class Solution(object):
-    '''
-    1,2,3,4,5
-    
-    
-    '''
-    
     def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
         prev, current = 1, 2
         if n <= 2: 
             return n
