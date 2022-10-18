3927e1ff8115ebd7c8e13b605822404d08d610d9
yohannes11 <johannes.neg@gmail.com>
2022-10-18 10:08:42 +0300

    Time: 16 ms (94.85%), Space: 13.5 MB (40.99%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,12 +1,22 @@
 class Solution(object):
     def rob(self, nums):
+        """
+        [2,1,1,2]
+                     
+                  
+        :type nums: List[int]
+        :rtype: int
+        """
         n = len(nums)
         if n <=2:
             return max(nums)
-        memo = {0:nums[0],1: max(nums[:2])}
+        current = nums[0]
+        current_max = max(nums[:2])
         for i in range(2,n):
-            memo[i] = max(memo[i-1],nums[i]+memo[i-2])
-        return memo[n-1]
+            temp = current_max
+            current_max = max(current_max,nums[i]+current)
+            current = temp
+        return current_max
             
             
         
