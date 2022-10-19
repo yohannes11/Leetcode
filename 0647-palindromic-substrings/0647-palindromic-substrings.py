151f3dd4d80582c3e59f6cfec02855dae850cbc3
yohannes11 <johannes.neg@gmail.com>
2022-10-19 14:42:44 +0300

    Time: 1818 ms (9.58%), Space: 117.6 MB (5.79%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def countSubstrings(self, s):
+        count = 0
+        n = len(s)
+        memo = {}
+        def dp(i,j):
+            if (i,j) in memo:
+                return memo[(i,j)]
+                
+            if i==j:
+                return True
+            else: 
+                if s[i] == s[j]:
+                    if i+1 == j:
+                        return True
+                    if (i+1,j-1) not in memo:
+                        memo[(i+1,j-1)] = dp(i+1,j-1)
+                    if memo[(i+1,j-1)] and s[i+1] == s[j-1]:
+                        return True
+                    else:
+                        return False
+
+                else:
+                    return False
+
+        for i in range(len(s)):
+            for j in range(i,len(s)):
+                if(dp(i,j)):
+                    count+=1
+        return count
+                
+            
+        
+            
+
+                
+            
+                
+            
