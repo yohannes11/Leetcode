b28582824f651fbf9cb1d5b15a9d395f50493730
yohannes11 <johannes.neg@gmail.com>
2022-10-20 14:25:00 +0300

    Time: 51 ms (8.44%), Space: 14 MB (9.66%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def numDecodings(self, s):
+        n = len(s)
+        dp = {n:1}
+        self.res = 0
+        
+        def dfs(i):
+            print()
+            if i in dp:
+                return dp[i]
+            if s[i] == '0':
+                return 0
+
+            self.res=dfs(i+1)
+            
+            if (i+1<n and (s[i] == '1' or s[i] == '2' and s[i+1] in '0123456')):
+                self.res+=dfs(i+2)
+            dp[i]=self.res
+            return self.res
+    
+        return dfs(0)
+            
+            
+            
+                
+               
+                
+            
+            
+                
+            
+            
+        
