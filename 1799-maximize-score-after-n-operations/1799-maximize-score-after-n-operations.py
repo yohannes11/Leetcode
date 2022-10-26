8ccb1787e9dddd3d2e00a63ec65aa1560f2ddaac
yohannes11 <johannes.neg@gmail.com>
2022-10-26 21:28:27 +0300

    Time: 3807 ms (62.86%), Space: 20.3 MB (81.71%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
@@ -6,18 +6,21 @@ class Solution(object):
         """
         memo = {}
         
+        
         def dfs(num):
-            if tuple(num) in memo:
-                return memo[tuple(num)]     
-            n = len(num)
+            if  tuple(num) in memo:
+                return memo[tuple(num)]
+            n = len(num)    
             res = 0
             for i in range(n-1):
                 for j in range(i+1,n):
                     num2 = num.copy()
                     num2.remove(num[i])
-                    num2.remove(num[j])        
-                    temp = (n//2) *math.gcd(num[i],num[j])+dfs(num2)              
+                    num2.remove(num[j])
+                    temp = (n//2)*math.gcd(num[i],num[j])+dfs(num2)    
                     res = max(res,temp)
             memo[tuple(num)] = res
-            return res     
+            return res
         return dfs(nums)
+
+            
