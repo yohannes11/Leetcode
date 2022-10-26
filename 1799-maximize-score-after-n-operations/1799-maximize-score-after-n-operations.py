4b273bb4b029b35b86b0cd5830fbe726dea9aa60
yohannes11 <johannes.neg@gmail.com>
2022-10-26 21:00:56 +0300

    Time: 3970 ms (57.72%), Space: 20 MB (82.86%) - LeetHub

---
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maxScore(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        memo = {}
+        
+        def dfs(num):
+            if tuple(num) in memo:
+                return memo[tuple(num)]     
+            n = len(num)
+            res = 0
+            for i in range(n-1):
+                for j in range(i+1,n):
+                    num2 = num.copy()
+                    num2.remove(num[i])
+                    num2.remove(num[j])        
+                    temp = (n//2) *math.gcd(num[i],num[j])+dfs(num2)              
+                    res = max(res,temp)
+            memo[tuple(num)] = res
+            return res     
+        return dfs(nums)
