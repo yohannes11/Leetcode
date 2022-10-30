5f96cc5b6895d90a08aa59edc2da214e17d206d3
yohannes11 <johannes.neg@gmail.com>
2022-10-30 05:15:36 +0300

    Time: 2200 ms (40.86%), Space: 15 MB (52.38%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -6,16 +6,15 @@ class Solution:
             return -1
         @functools.lru_cache(None)
         def dp(day, cut):
-            if day ==1 :
+            if day ==1:
                 return max(jobDifficulty[cut:])
+            
             maxsofar = 0
             answer = float('inf')
-            
-            for j in range(cut, numjobs - day + 1):
+            for j in range(cut,numjobs- day +1):
                 maxsofar = max(maxsofar,jobDifficulty[j])
-                answer = min(answer, maxsofar+dp(day-1, j+1))
+                answer = min(answer, maxsofar+dp(day-1,j+1))
             return answer
         
         return dp(d,0)
-            
             
