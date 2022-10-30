ac158d7d258a8f705adcb67ea2200e7618186808
yohannes11 <johannes.neg@gmail.com>
2022-10-30 05:04:01 +0300

    Time: 2471 ms (31.10%), Space: 14.9 MB (62.51%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -1,24 +1,21 @@
+import functools
 class Solution:
     def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
         numjobs = len(jobDifficulty)
-        memo = {}
         if numjobs < d:
             return -1
+        @functools.lru_cache(None)
         def dp(day, cut):
-            if ((day,cut) in memo):
-                return memo[(day,cut)]
-            if day == 1:
+            if day ==1 :
                 return max(jobDifficulty[cut:])
-            
             maxsofar = 0
             answer = float('inf')
-            for j in range(cut, numjobs -day +1):
-                maxsofar = max(jobDifficulty[cut:j+1])
-                answer = min(answer, maxsofar+dp(day-1,j+1))
-            memo[(day,cut)] = answer
-            return answer
-        return dp(d,0)
-                
             
-                
-                
+            for j in range(cut, numjobs - day + 1):
+                maxsofar = max(maxsofar,jobDifficulty[j])
+                answer = min(answer, maxsofar+dp(day-1, j+1))
+            return answer
+        
+        return dp(d,0)
+            
+            
