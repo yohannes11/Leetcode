ed1ffb052afe8a0ec873ac825799ac564c66c87d
yohannes11 <johannes.neg@gmail.com>
2022-11-01 21:29:25 +0300

    Time: 123 ms (21.08%), Space: 19.3 MB (8.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(len(s)):
             if s[i].isalnum():output_str.append(s[i].lower())
         output_str = ''.join(output_str)
-        print(output_str)
         left_ptr, right_ptr = 0, len(output_str) - 1
         
         while left_ptr<= right_ptr:
